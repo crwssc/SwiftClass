2d187b04ed8241e8cafe583dbf01eeb98b6af5b1
croissance <myadressemailisthat@gmail.com>
2021-10-06 08:46:40 +0200

    conversion de type

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,16 +1,8 @@
 import Foundation
 
-func magicBox(data: Int) -> String {
-    var message = ""
-    
-    if data > 18 {
-        message = "Bienvenue fréro"
-    } else {
-        message = "Vous n'êtes pas autorisé à louer une voiture"
-    }
-    
-    return message
-}
 
-magicBox(data: 52)
-magicBox(data: 13)
+// Conversion de Type
+
+let fullBattery = 100
+
+var percentage = Double(fullBattery) - 21.34
