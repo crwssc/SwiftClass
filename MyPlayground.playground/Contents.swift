cd2b7f6a2b56cee1a0e3db82b2d3b178152e3e96
croissance <myadressemailisthat@gmail.com>
2021-11-03 21:05:23 +0100

    Opérateurs unitaires et composés

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,11 +1,27 @@
 import Foundation
 
-var possibleNumber = "456"
+// l'opérateur d'affectation permet d'assigner des valeurs à des variables, là on voit par exemple avec les tupes que l'on peut le faire de deux manières différentes
 
-if let number = Int(possibleNumber) {
-    print(number)
-} else {
-    
-}
+var name = "Quentin"
+let (user, password) = ("Quentin", "1234")
 
-// autre façon de faire la condition
+// Oprérateurs arithmétiques ->
+
+// + - * /
+
+var a = 3
+var b = 5
+
+a - b
+
+// on ne peut pas utiliser les opératuers avec n'importe quel types de variables
+
+// Opérateurs unitaires
+
+let number = 12
+let oposite = -number
+
+// Opérateurs commposés
+
+var 
+
