9492a5d3367120529ac35e8c631150a022710b3d
croissance <myadressemailisthat@gmail.com>
2021-11-08 09:11:06 +0100

    opérateurs d'intervalle

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,18 @@
 import Foundation
 
-// Opérateurs de combinaison d'optionnelle
+// Opérateurs d'intervalles
 
-var adress: String?
+for index in 1...5 {
+    print(index)
+}
 
-var textToPrint = adress ?? "Adresse inconnue"
+// 1...5 1 et 5 inclus
+// 1..<5 5 exclus
 
-// c'est la même chose que dans le cas précédent mais avec moins de ligne, on met deux points d'interrogation puis entre guillemets le message qui doit s'afficher dans le cas où la varibale n'a pas de valeurs
+var names = ["Quentin", "Lucas", "Nicolas"]
 
-print(adress ?? "..." )
+names.count
 
-// Pareil avec la fonction print par exemple
-
+for index in 0..<names.count {
+    print(names[index])
+}
