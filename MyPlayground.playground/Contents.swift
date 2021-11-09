2b87fd000d6ca46d2943675dd458c19c0b1bc5c7
croissance <myadressemailisthat@gmail.com>
2021-11-09 22:17:21 +0100

    comment créer un tableau

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,19 @@
 import Foundation
 
-// Utiliser fistIndex
+// Tableaux
 
-var name = "Quentin Dev Libre"
+// un tableau est un groupe de plusieurs valeurs du même type
 
-name.firstIndex(of: "D")
+// différentes manière de faire
+
+var array: Array<Int> // ici on n'a pas donner de valeur à notre tableau puisque qu'on a pas mis de =  en dessous aussi d'ailleurs
+
+var array2: [Int]
+
+// On ne peut pas changer le type des données d'un tableau en cours du programme
+
+var array3 = [Int]() // Ça par contre c'est un tableau avec aucun élément à l'intérieur
+
+var array4 = Array<Int>()
+
+// Voici les 4 manières de définir un tableau
