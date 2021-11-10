61e1b1aaa33cb776a4bfebd50a22e540280f746f
croissance <myadressemailisthat@gmail.com>
2021-11-10 09:07:18 +0100

    Ensembles - Objets Hashables

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,53 +1,14 @@
 import Foundation
 
-// Tableaux
+// Ensembles (Sets)
 
-// un tableau est un groupe de plusieurs valeurs du même type
+// Un set est comme un tableau mais sans doublons et ordres
 
-// différentes manière de faire
+// Les objets doivent être hashables, deux objets sont hashables si lorsqu'ils sont égaux ils ont le même hashé
 
-var array: Array<Int> // ici on n'a pas donner de valeur à notre tableau puisque qu'on a pas mis de =  en dessous aussi d'ailleurs
+let maString = "Quentin"
+let maDeuxièmeString = "Quentin"
 
-var array2: [Int]
+maString.hashValue
+maDeuxièmeString.hashValue
 
-// On ne peut pas changer le type des données d'un tableau en cours du programme
-
-var array3 = [Int]() // Ça par contre c'est un tableau avec aucun élément à l'intérieur
-
-var array4 = Array<String>()
-
-// Voici les 4 manières de définir un tableau
-
-array3.append(5)
-array4.append("Yes Man")
-
-// autres manières de créer des tableaux (en utilisant les propositions de l'autocomplétion )
-// Ici par exemple, on crée un tableau qui aura 32 valeurs et chaque valeurs sera le 0
-
-var listOfPixels = Array(repeating: 0, count: 32)
-
-// Manière la plus courante de créer un tableau
-
-var toDoList = ["Clean the house", "Sign the contract", "Pay the apple"]
-
-
-
-// Accéder à un tableau et le modifier
-
-toDoList[1] // on veut accéder à l'indice 1
-
-toDoList[0...1] // on veut accéder aux deux premiers éléments
-
-toDoList.insert("Do homework", at: 1)
-
-
-
-toDoList.remove(at : 2)
-
-// Parcourir un tableau
-
-toDoList.append("Nouvelle tache")
-
-for (index, task) in toDoList.enumerated() {
-    print("\(task)\t indice : \(index)")
-}
