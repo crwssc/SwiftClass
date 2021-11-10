936d36755601454d7ec6b8802b62be1274875317
croissance <myadressemailisthat@gmail.com>
2021-11-10 20:26:21 +0100

    comparaison d'ensemble

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -22,3 +22,16 @@ ens1.symmetricDifference(ens2)
 ens1.union(ens2)
 ens1.subtracting(ens2)
 ens2.subtracting(ens1)
+
+//Comparaison d'ensembles
+
+ens1.isSubset(of: ens2) // trad : Est-ce que ens1 est un sous ensemble de l'ensemble 2
+
+ens1.isSuperset(of: ens2) // inverse
+
+ens2.isStrictSuperset(of: ens1) // pareil que le 1 met supérieur strict
+
+ens2.isDisjoint(with: ens1)
+
+
+
