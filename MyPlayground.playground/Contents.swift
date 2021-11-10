1c758528d0e292856c80cf0ebc0b99989d8d2641
croissance <myadressemailisthat@gmail.com>
2021-11-10 22:11:31 +0100

    les deux manières de créer un dictionnaire

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,11 @@ import Foundation
 
 // Dictionnaires
 
+var MathDictionary: Dictionary<String, Float>
+
+MathDictionary = Dictionary<String, Float>()
+
+// 2 eme façon de créer
+
+let monDico = [String: Float]()
 
