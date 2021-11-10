771a7fdc4ddc5082958f8142e7d7e727ce3dc32d
croissance <myadressemailisthat@gmail.com>
2021-11-10 09:18:38 +0100

    méthodes pour les ensembles (1)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,7 +7,21 @@ import Foundation
 var lettres = Set<Character>()
 
 lettres.insert("a")
-
+lettres.insert("b")
 print(lettres)
 
+// Quelques méthodes pour les ensembles
 
+var musicalGenders: Set<String> = Set<String>()
+
+musicalGenders.isEmpty
+
+musicalGenders.insert("Hip hop")
+musicalGenders.insert("Rock")
+
+musicalGenders.remove("Rock")
+musicalGenders.remove("Classical")
+
+for genre in musicalGenders.sorted() {
+    print(genre)
+}
