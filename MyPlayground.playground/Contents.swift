a3cd009152dd8c11b5248115dc9b54176cf817ef
croissance <myadressemailisthat@gmail.com>
2021-11-11 11:20:28 +0100

    utilisation if et else

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,11 @@ import Foundation
 
 // if et else
 
+var subscriptionPlan = 39
+var connected = true
 
+if (subscriptionPlan >= 39) {
+    print ("Access to premium courses : ✅")
+} else {
+    print("Access to regular courses : ✅")
+}
