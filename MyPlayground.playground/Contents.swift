8cc1da1b7bf5198d3094036fb0a5b70155c8ed1e
croissance <myadressemailisthat@gmail.com>
2021-11-11 13:19:14 +0100

    différentes manières switch case

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,25 +2,13 @@ import Foundation
 
 // if et else
 
-var subscriptionPlan = 39
+var subscriptionPlan = 19
 var connected = true
 
-if (subscriptionPlan >= 39) {
-    print ("Access to premium courses : ✅")
-} else if (subscriptionPlan >= 19) {
-    print("Access to regular courses : ✅")
-} else {
-    print("Access denied, you should pay to have access to courses")
-}
-
-// si deux conditions sont vérifiées l'ordi ne regarde que les première
-
-// Autre syntaxe avec le Switch case
-
 switch subscriptionPlan {
-case 39:
+case 19...39:
     print ("Access to premium courses : ✅")
-case 19:
+case 9:
     print("Access to regular courses : ✅")
 default:
     print("Access denied, you should pay to have access to courses")
