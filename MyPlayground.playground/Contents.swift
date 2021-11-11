24ee7edb15e036dd8589434419f190fce1bdb045
croissance <myadressemailisthat@gmail.com>
2021-11-11 11:33:05 +0100

    switch case

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -12,3 +12,16 @@ if (subscriptionPlan >= 39) {
 } else {
     print("Access denied, you should pay to have access to courses")
 }
+
+// si deux conditions sont vérifiées l'ordi ne regarde que les première
+
+// Autre syntaxe avec le Switch case
+
+switch subscriptionPlan {
+case 39:
+    print ("Access to premium courses : ✅")
+case 19:
+    print("Access to regular courses : ✅")
+default:
+    print("Access denied, you should pay to have access to courses")
+}
