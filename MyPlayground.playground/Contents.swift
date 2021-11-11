9e9c3c51cc63179e2d403e2a65ac13062239685e
croissance <myadressemailisthat@gmail.com>
2021-11-11 13:41:32 +0100

    nv chap : les fonctions

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,5 @@
 import Foundation
 
-var point = (0, 0)
+// Fonctions
 
-switch point {
-case (0, 0):
-    print("Point à l'origine")
-case (_, 0):
-    print("Point à laxe dex X")
-case (0, _):
-    print("Point à laxe dex Y")
-default:
-    print("Point quelconque")
-}
+
