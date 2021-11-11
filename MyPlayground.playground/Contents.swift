34199c2f847237d58b85dd7d7078a9cc34e2446e
croissance <myadressemailisthat@gmail.com>
2021-11-11 09:19:44 +0100

    autre cas de boucle for

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,11 +2,12 @@ import Foundation
 
 // Boucles et conditions
 
-// for in c'est quand on connait le nbre de tours que l'on doit faire, pour faire une action un certain nbre de fois
+let base = 2
+let power = 5
+var result = 1
 
-var cities = ["La": "Los Angeles","PA": "Paris", "BE": "Berlin"]
-
-for (code, city) in cities {
-    print(city)
-    print(code)
+for _ in 1...power {
+    result *= base
 }
+
+print(result)
