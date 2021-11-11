0455f775873787abf392cbf3d231d71ba7c13098
croissance <myadressemailisthat@gmail.com>
2021-11-11 09:14:12 +0100

    parcourir un tableau avec for in

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,6 +4,8 @@ import Foundation
 
 // for in c'est quand on connait le nbre de tours que l'on doit faire, pour faire une action un certain nbre de fois
 
-for index in 10..<15 {
-    print(index)
+var cities = ["Los Angeles", "Paris", "Berlin"]
+
+for city in cities {
+    print(city)
 }
