8e996dde432ee6de2f90513bb2da4b1da462d2cf
croissance <myadressemailisthat@gmail.com>
2021-11-11 11:27:39 +0100

    utilisation else if

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,6 +7,8 @@ var connected = true
 
 if (subscriptionPlan >= 39) {
     print ("Access to premium courses : ✅")
-} else {
+} else if (subscriptionPlan >= 19) {
     print("Access to regular courses : ✅")
+} else {
+    print("Access denied, you should pay to have access to courses")
 }
