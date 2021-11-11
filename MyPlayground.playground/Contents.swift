d933e572299cc0c4a19fe60a82f87f9b1c06e3a6
croissance <myadressemailisthat@gmail.com>
2021-11-11 11:08:43 +0100

    utilisation de la boucle while

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,13 +1,14 @@
 import Foundation
 
-// Boucles et conditions
+// on utilise les boucles while lorsque l'on sait pas à l'avance le nombre d'ittération que l'on va faire
 
-let base = 2
-let power = 5
-var result = 1
+let base: Double = 2
+var power: Double = 5
 
-for _ in 1...power {
-    result *= base
+let numberToStore = 57
+
+// on veut créer une fonction qui nous permette de savoir combien avons nous besoin de bits pour créer tel nombre
+
+while pow(base, power) <= Double(numberToStore) {
+    power += 1
 }
-
-print(result)
