85216236aed79d8a87856005543fb6a33cb2491f
croissance <myadressemailisthat@gmail.com>
2021-11-11 11:12:01 +0100

    autre façon d'utiliser while

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,10 @@
 import Foundation
 
-// on utilise les boucles while lorsque l'on sait pas à l'avance le nombre d'ittération que l'on va faire
+var success: Bool?
+var numberOfTry = 0
 
-let base: Double = 2
-var power: Double = 5
+repeat {
+    numberOfTry += 1
+} while (Int.random(in: 0..<10) != 7)
 
-let numberToStore = 57
-
-// on veut créer une fonction qui nous permette de savoir combien avons nous besoin de bits pour créer tel nombre
-
-while pow(base, power) <= Double(numberToStore) {
-    power += 1
-}
+print (numberOfTry)
