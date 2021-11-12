eb5b718a00bd0c54780ee37d7748fe2b63a1d496
croissance <myadressemailisthat@gmail.com>
2021-11-12 18:39:33 +0100

    add a label in the function

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,26 +1,8 @@
 import Foundation
 
-func minMax(array: [Int]) -> (min: Int, max: Int)? {
-    
-    if (array.isEmpty) {
-        return nil
-    }
-    
-    var currentMin = array[0]
-    var currentMax = array[0]
-    
-    for value in array {
-        if (value > currentMax) {
-            currentMax = value
-        } else if (value < currentMin) {
-            currentMin = value
-        }
-    }
-    return (currentMin, currentMax)
+func sayHelloTo(to name : String, from city: String) {
+    print("Hello \(name) from \(city)")
 }
 
-var monTab = [Int]()
-
-
-
+sayHelloTo(to: "Quentin", from: "Paris")
 
