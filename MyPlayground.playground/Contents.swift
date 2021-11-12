16475606ce59058afdfe4970eaa7de5229879be5
croissance <myadressemailisthat@gmail.com>
2021-11-12 19:31:04 +0100

    use functions in functions

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,15 +1,25 @@
 import Foundation
 
-func multiply(op1: Int, op2: Int) -> String {
-    return "\(op1) x \(op2) = \(op1 * op2)"
+func chooseDirection(left: Bool) {
+    var position = 0
+    
+    func goLeft() {
+        position += 1
+    }
+    
+    func goRight() {
+        position -= 1
+    }
+    
+    if (left == true) {
+        goLeft()
+    } else {
+        goRight()
+    }
+    
+    print(position)
 }
 
-func sub(op1: Int, op2: Int) -> String {
-    return "\(op1) - \(op2) = \(op1 * op2)"
-}
-
-func displayCalcul(op1: Int, op2: Int, calculFonction: (Int, Int) -> String) {
-    print(calculFonction(op1, op2))
-}
-
-displayCalcul(op1: 4, op2: 6, calculFonction: multiply(op1:op2:))
+chooseDirection(left: true)
+chooseDirection(left: true)
+chooseDirection(left: false )
