e23b4d19dc367fa2049effad9a97adfd61e7c0c0
croissance <myadressemailisthat@gmail.com>
2021-11-12 18:05:59 +0100

    functions with two arguments

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,9 +2,11 @@ import Foundation
 
 // Fonctions
 
-func sayHello(name: String) {
-    print("Hello \(name)")
+func sayHello(name1: String, name2: String) {
+    print("Hello \(name1) and \(name2)")
 }
 
-sayHello(name: "Quentin")
-sayHello(name: "Luca")
+sayHello(name1: "Quentin", name2: "Nico")
+sayHello(name1: "Luca", name2: "David")
+
+
