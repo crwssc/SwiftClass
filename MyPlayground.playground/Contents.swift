4dcafef0a5186304323b95c26633438ad99464a4
croissance <myadressemailisthat@gmail.com>
2021-11-12 18:18:20 +0100

    utiliser des tupes avec les fonctions

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-func passwordFor(UID: Int) -> String {
-    return "1234"
+func passwordFor(UID: Int) -> (name: String, password: String) {
+    return ("Mon nom", "1234")
 }
 
 func accessGranted(userID : Int, password: String) -> Bool {
-    if (password == passwordFor(UID: userID)) {
+    if ( password == passwordFor(UID: userID).password ) {
         return true
     } else {
         return false
