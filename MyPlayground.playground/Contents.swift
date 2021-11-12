e57d3d9062c7b5b28eb5ec1e05ab931374c172cb
croissance <myadressemailisthat@gmail.com>
2021-11-12 19:55:21 +0100

    closures

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,30 +1,10 @@
 import Foundation
 
-func chooseDirection(left: Bool) -> (Int) -> Int {
-    
-    func goLeft(position: Int) -> Int {
-        return position + 1
-    }
-    
-    func goRight(position: Int) -> Int {
-        return position - 1
-    }
-    
-    if (left) {
-        return goLeft
-    } else {
-        return goRight
-    }
-    
-}
+// les closures
 
-var maFonction: (Int) -> Int
 
-maFonction = chooseDirection(left: true)
+var names = ["Quentin", "Nico", "Lucas", "Alex"]
 
-maFonction(3)
-maFonction(4)
-
-maFonction = chooseDirection(left: false)
-
-maFonction(5)
+names.sort(by: {(s1 : String, s2: String) -> Bool in
+    return s1 < s2
+})
