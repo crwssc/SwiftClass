ae24ac7d285140121c178df65ece782fd00250b0
croissance <myadressemailisthat@gmail.com>
2021-11-12 18:25:14 +0100

    exemple de fonction en utilisant les tuples

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,15 +1,21 @@
 import Foundation
 
-func passwordFor(UID: Int) -> (name: String, password: String) {
-    return ("Mon nom", "1234")
-}
-
-func accessGranted(userID : Int, password: String) -> Bool {
-    if ( password == passwordFor(UID: userID).password ) {
-        return true
-    } else {
-        return false
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    
+    for value in array {
+        if (value > currentMax) {
+            currentMax = value
+        } else if (value < currentMin) {
+            currentMin = value
+        }
     }
+    return (currentMin, currentMax)
 }
 
-accessGranted(userID: 001, password: "1234")
+var monTab = [1, 6, 8, 3, 9, -3]
+
+var mesMinEtMAx = minMax(array: monTab)
+
+mesMinEtMAx.min
