06badae4592f16b4f22ffe2b45f806cdcc4171bc
croissance <myadressemailisthat@gmail.com>
2021-11-12 18:12:23 +0100

    use a function in a function

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,11 +1,15 @@
 import Foundation
 
+func passwordFor(UID: Int) -> String {
+    return "1234"
+}
+
 func accessGranted(userID : Int, password: String) -> Bool {
-    if (password == "azerty") {
+    if (password == passwordFor(UID: userID)) {
         return true
     } else {
         return false
     }
 }
 
-accessGranted(userID: 001, password: "azerty")
+accessGranted(userID: 001, password: "1234")
