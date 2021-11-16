3f61d4a8ec2cfdfa6599471940352abcc1ddffa2
croissance <myadressemailisthat@gmail.com>
2021-11-16 09:06:35 +0100

    propriété en lecture seule

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,40 +1,14 @@
 import Foundation
 
-// Les Propriétés
-// Computing poperties
-// Property Observed
+// Propriétés en lecture seule
 
-struct Point {
-    var x: Float
-    var y: Float
-}
-
-struct Size {
-    var height: Float
-    var width: Float
-}
-
-struct Rectangle {
-    var origine = Point(x: 0.0, y: 0.0)
-    var size = Size(height: 15.0, width: 10.0)
-    
-    var center: Point {
-        get {
-            let centerX = origine.x + size.width/2
-            let centerY = origine.y + size.height/2
-            return Point(x: centerX, y: centerY)
-        }
-        set(newCenter) {
-            origine.x = newCenter.x - size.width / 2
-            origine.y = newCenter.y - size.height / 2
-        }
+struct Cube {
+    var size: Float = 0.0
+    var volume: Float {
+        return pow(size, 3)
     }
 }
 
-var myRect = Rectangle()
-myRect.center.x
-myRect.center.y
+var monCube = Cube()
 
-myRect.center = Point(x: 0, y: 0)
 
-myRect.origine.y
