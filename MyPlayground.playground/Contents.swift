6c21416b748099750778407d98cdc15418136d40
croissance <myadressemailisthat@gmail.com>
2021-11-16 09:00:47 +0100

    computing properties &property observed

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 // Les Propriétés
+// Computing poperties
+// Property Observed
 
 struct Point {
     var x: Float
@@ -9,14 +11,30 @@ struct Point {
 
 struct Size {
     var height: Float
-    var widht: Float
+    var width: Float
 }
 
 struct Rectangle {
     var origine = Point(x: 0.0, y: 0.0)
-    var size = Size(height: 15.0, widht: 10.0)
+    var size = Size(height: 15.0, width: 10.0)
     
     var center: Point {
-        
+        get {
+            let centerX = origine.x + size.width/2
+            let centerY = origine.y + size.height/2
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origine.x = newCenter.x - size.width / 2
+            origine.y = newCenter.y - size.height / 2
+        }
     }
 }
+
+var myRect = Rectangle()
+myRect.center.x
+myRect.center.y
+
+myRect.center = Point(x: 0, y: 0)
+
+myRect.origine.y
