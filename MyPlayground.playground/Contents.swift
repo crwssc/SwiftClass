d3abb8fa7d2a9dc0b516c033dd1f4faa43b4824f
croissance <myadressemailisthat@gmail.com>
2021-11-16 11:31:52 +0100

    créer un 2 eme constructeur

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -11,7 +11,15 @@ class Color {
         self.blue = blue
     }
     
-    
+    init(white: Double) {
+        self.red = white
+        self.green = white
+        self.blue = white
+    }
 }
 
 var prple = Color(red: 0.5, green: 0, blue: 0.5)
+
+var grey = Color(white: 0.2)
+
+
