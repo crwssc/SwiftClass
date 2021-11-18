5414a6c983cc979347b6c86eac9321b0d2e658d4
croissance <myadressemailisthat@gmail.com>
2021-11-18 20:25:08 +0100

    étendre des types créés par nous même

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,12 +2,16 @@ import Foundation
 
 // Les extensions
 
-extension Double {
-    var km: Double { return self * 1_000 }
-    var m: Double { return self }
-    var mm: Double { return self / 1_000 }
+struct Rect {
+    var height: Double
+    var width: Double
 }
 
-var distance: Double = 14.km + 250.m
+extension Rect {
+    init(surface: Double) {
+        width = sqrt(surface)
+        height = width
+    }
+}
 
-distance
+var monRectangle = Rect
